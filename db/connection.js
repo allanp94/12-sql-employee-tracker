d549ebdd7b279f315c10d04ed8b90996e02b26c6
allan.pirillis <allan.p94@gmail.com>
2021-05-26 21:33:15 -0500

    created the basic file structure, install some of the npm packages needed for the application, and begun creating the necessary files for the database

---
+++ db/connection.js
@@ -0,0 +1,16 @@
+const mysql = require("mysql2");
+
+require("dotenv").config();
+
+//connect to database
+const db = mysql.createConnection(
+  {
+    host: process.env.DB_HOST,
+    user: process.env.DB_NAME,
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME,
+  },
+  console.log("Connected to the Employee Tracker database")
+);
+
+module.exports = db;
---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO department ( name)
+VALUES 
+(1, 'Fabrication');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+('Assembly', 40.300, 1); 
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES
+('Allan', 'Pirillis', 1);
